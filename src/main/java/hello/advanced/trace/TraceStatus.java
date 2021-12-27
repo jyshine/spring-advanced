2808767f21f6897a5d75cc7b5323027cdc22f5a0
신준영 <june@thecommerce.co.kr>
2021-12-28 01:51:29 +0900

    feat:log trace, threadLocal test

---
+++ src/main/java/hello/advanced/trace/TraceStatus.java
@@ -0,0 +1,26 @@
+package hello.advanced.trace;
+
+public class TraceStatus {
+
+    private TraceId traceId;
+    private Long startTimeMs;
+    private String message;
+
+    public TraceStatus(TraceId traceId, Long startTimeMs, String massage) {
+        this.traceId = traceId;
+        this.startTimeMs = startTimeMs;
+        this.message = massage;
+    }
+
+    public TraceId getTraceId() {
+        return traceId;
+    }
+
+    public Long getStartTimeMs() {
+        return startTimeMs;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
